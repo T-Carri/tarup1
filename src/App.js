1546a6f77f501780277cba99ab94a1a2261723dc
T-Carri <bryan_narutito@hotmail.com>
2021-09-14 20:45:37 -0500

    genesis

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Home from './pages/home/Home.jsx'
+import './App.css';
+
+function App() {
+  return (
+    <div>
+<Home/>
+      <h1> hello tarup </h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,80 @@
+import React, {Component, useState, useEffect } from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import  './Home.css'
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+    width: 50,
+    height: 50
+  },
+  title: {
+
+    
+    flexGrow: 1
+    ,
+  },
+}));
+
+function ButtonAppBar() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+          <div className="logoTarup">
+<img className="mainLogo" src={process.env.PUBLIC_URL + '/mainlogo.png'} ></img>
+</div>
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            Tarup 
+
+
+            
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
+ 
+//import Login
+//import Register
+/*function barraBlanca() {
+     return (
+        
+         
+     )
+ }*/
+ 
+ export default class Home extends Component {
+     
+    constructor(props) {
+        super(props)
+     this.state = {
+             LOGIN:false, 
+             REGISTRA: false,
+             info: true }
+    }
+      render() {
+        return (
+            <ButtonAppBar/>
+           
+           //<h1>helloou</h1>
+           //{areadeAcceso}
+           //{efemeride}
+
+        )
+    }
+}
