7481a7fdc5d5cdd1456821d1f3a6e21455dd039c
T-Carri <bryan_narutito@hotmail.com>
2021-09-24 21:27:11 -0500

    2

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -7,6 +7,74 @@ import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import  './Home.css'
+
+function ButtonAppBar() {
+  const classes = useStyles();
+
+
+
+
+
+
+
+
+  return (
+    <div className="LAbarraBlanca">
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+          <div className="logoTarup">
+<img className="mainLogo" src={process.env.PUBLIC_URL + '/mainlogo.png'} ></img>
+</div>
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            Tarup 
+          </Typography>
+          <Button {()=>{
+            return(componentesDeInicio.component1);
+          }} color="inherit">Login</Button>
+          <Button {prop.componentesDeInicio.component2} color="inherit">Registrate</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
+ 
+export default class Home extends Component {
+     
+  const componentesDeInicio= {
+       component1: <Login/>,
+       component2: <Registrar/>,
+       component3: <Info/>
+     }  
+    
+
+    render() {
+      return (
+
+      <div>
+        {this.props.componentesDeInicio}
+      </div>
+         //<h1>helloou</h1>
+         //{areadeAcceso}
+         //{efemeride}
+
+      )
+  }
+}
+
+//import Login
+//import Register
+/*function barraBlanca() {
+     return (
+        
+         
+     )
+ 
+}
+
+
+PEDAZO DE CODIGO QUE CORTE DEL ESTILO CON MATERIAL UI
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
@@ -24,57 +92,9 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-function ButtonAppBar() {
-  const classes = useStyles();
-
-  return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-          <div className="logoTarup">
-<img className="mainLogo" src={process.env.PUBLIC_URL + '/mainlogo.png'} ></img>
-</div>
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            Tarup 
 
 
-            
-          </Typography>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
-      </AppBar>
-    </div>
-  );
-}
+
+*/
  
-//import Login
-//import Register
-/*function barraBlanca() {
-     return (
-        
-         
-     )
- }*/
  
- export default class Home extends Component {
-     
-    constructor(props) {
-        super(props)
-     this.state = {
-             LOGIN:false, 
-             REGISTRA: false,
-             info: true }
-    }
-      render() {
-        return (
-            <ButtonAppBar/>
-           
-           //<h1>helloou</h1>
-           //{areadeAcceso}
-           //{efemeride}
-
-        )
-    }
-}
